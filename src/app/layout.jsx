60f4b8230fcb188fb28ff3abd52751ce2c6ce78d
AnshYadav2412 <2412anshyadav@gmail.com>
2026-02-13 12:56:50 +0530

    Project Setup and Layout setup done

---
+++ src/app/layout.jsx
@@ -0,0 +1,47 @@
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+import "./globals.css";
+import LightRays from "@/component/LightRays";
+
+const schibsted_Grotesk = Schibsted_Grotesk({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const martian_Mono = Martian_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Dev Event",
+  description: "The Hub for every Dev event you mustn't miss",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${schibsted_Grotesk.variable} ${martian_Mono.variable} min-h-screen antialiased`}
+      >
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center"
+            raysColor="#ffffff"
+            raysSpeed={1}
+            lightSpread={0.5}
+            rayLength={3}
+            followMouse={true}
+            mouseInfluence={0.1}
+            noiseAmount={0}
+            distortion={0}
+            className="custom-rays"
+            pulsating={false}
+            fadeDistance={1}
+            saturation={1}
+          />
+        </div>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> You can't Miss.
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetup, and Conferences, All in one place.
+      </p>
+    </section>
+  );
+};
+
+export default Page;
